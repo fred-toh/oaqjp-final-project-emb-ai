1e43e9cfc1765714b6cbf8e74476910e6b8b9c86
fred-toh <fredtoh@hotmail.com>
2024-06-02 00:12:15 -0400

    Task 8 completed. All tasks completed. Final push.

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+''' Executing this function initiates the application of emotion
+    detection to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+
 # Import the libraries and packages
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector as detect
@@ -7,11 +12,20 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emo_detector():
+    ''' This code receives the text from the HTML interface as `textToAnalyze, 
+        which is used as an argument to the `emotion_detector()` function,
+        alias is `detect`. The returned response is a json dict, which is provided
+        to the HTML interface as a formatted text, displaying each emotion's score
+        and also the dominant emotion.
+    '''
     # Param from index.html
     text_to_analyze = request.args.get('textToAnalyze')
     resp = detect(text_to_analyze)
 
-    return f"For the given statement, the system response is " \
+    if resp['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
+    return "For the given statement, the system response is " \
             + f"'anger': {resp['anger']}, " \
             + f"'disgust': {resp['disgust']}, " \
             + f"'fear': {resp['fear']}, " \
@@ -21,10 +35,15 @@ def emo_detector():
 
 @app.route("/")
 def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
     return render_template("index.html")
 
 @app.errorhandler(500)
 def invalid_input():
+    ''' This is to handle errors. 
+    '''
     return ({"message": "Invalid input! Please try again."}, 500)
 
 app.run(host = "0.0.0.0", port = "5000")
